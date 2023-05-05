2315bc53cff61e6e38d66a20a042a08d772c4471
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-05-05 16:29:57 +0600

    pages upgrade

--- layouts/form.vue
+++ layouts/form.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="wrapper" class="flex flex-col justify-between h-screen">
+
         <FormHeader />
+
         <slot />
+        
         <FormFooter />
     </div>
 </template>
---
+++ pages/pages-upgrade.vue
@@ -0,0 +1,3 @@
+<template>
+    <ContentPagesUpgrade />
+</template>
