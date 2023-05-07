1cd7bd70093fab1e6fc8f56ddf2f49a585d728a9
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-05-07 18:13:14 +0600

    adding tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
     app: {
         head: {
             link: [
                 {rel: "stylesheet", href: "css/style.css"},
                 {rel: "stylesheet", href: "css/icons.css"},
                 {rel: "stylesheet", href: "css/uikit.css"},
+                // {rel: "stylesheet", href: "css/main.css"},
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
                 { rel: 'stylesheet', type: 'text/css', href: 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css' },
             ],
