e4fda51609f58d3fc1abefb3d285a0a7083e14c3
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-05-09 14:54:22 +0600

    createGroupComponent

--- components/Content/CreateGroup.vue
+++ components/Content/CreateGroup.vue
@@ -1,32 +1,83 @@
 <template>
     <div class="main_content">
+            <div class="mcontainer">
 
-<div class="mcontainer">
+                <!--  breadcrumb -->
+                <div class="breadcrumb-area py-0">
+                    <div class="breadcrumb">
+                        <ul class="m-0">
+                            <li>
+                                <a href="index.html">Groups</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">Create New Group </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-    <div class="bg-white max-w-4xl mx-auto md:p-10 p-6 relative rounded-md shadow">
-        
-        <div class="md:space-y-6 space-y-4 text-gray-400 md:text-lg">
-             
-            <div class="md:leading-8 leading-7"> Updated July 09, 2021</div>
-            <div class="font-semibold md:text-2xl text-xl text-gray-700"> Privacy Policy </div>
-            <div class="md:leading-8 leading-7"> Version 2.1, Revision 4</div>
 
-            <div class="font-semibold md:text-2xl text-2xl text-gray-700 md:pt-12 pt-10"> Using our services </div>
-            <div class="md:leading-8 leading-7"> You must follow any policies made available to you within the Services.</div>
-            <div class="md:leading-8 leading-7"> Don't misuse our Services. For example, don't interfere with our Services or try to access them using a method other than the interface and the instructions that we provide. You may use our Services only as permitted by law, including applicable export and re-export control laws and regulations. We may suspend or stop providing our Services to you if you do not comply with our terms or policies or if we are investigating suspected misconduct. </div>
-            <div class="md:leading-8 leading-7"> Using our Services does not give you ownership of any intellectual property rights in our Services or the content you access. You may not use content from our Services unless you obtain permission from its owner or are otherwise permitted by law. These terms do not grant you the right to use any branding or logos used in our Services. Don't remove, obscure, or alter any legal notices displayed in or along with our Services. </div>
+                <!-- create page-->
+                <div class="max-w-2xl m-auto shadow-md rounded-md bg-white lg:mt-20">  
+ 
+                    <!-- form header -->
+                    <div class="text-center border-b border-gray-100 py-6">
+                        <h3 class="font-bold text-xl"> Create New Group </h3>
+                    </div>
+
+                    <!-- form body -->
+                    <div class="p-10 space-y-7">
+
+                        <div class="line">
+                            <input class="line__input" id="" name="" type="text" onkeyup="this.setAttribute('value', this.value);" value="" autocomplete="off">
+                            <span for="username" class="line__placeholder"> Group Name </span>
+                        </div>
+
+                        <div>
+                            <label for="" class="font-semibold text-base"> Group Category </label>
+                            <select id="" name=""  class="shadow-none selectpicker with-border">
+                                <option value="1">Technology</option>
+                                <option value="2">Cars and Vehicles</option>
+                                <option value="3">Comedy</option>
+                                <option value="4">Economics and Trade</option>
+                                <option value="5">Education</option>
+                                <option value="6">Entertainment</option>
+                                <option value="7">Movies & Animation</option>
+                                <option value="8">Gaming</option>
+                                <option value="9">History and Facts</option>
+                                <option value="10">Live Style</option> 
+                                <option value="0">Other</option>
+                           </select>
+                        </div>
+
+                        <div class="line h-32"> 
+                            <textarea class="line__input h-32"  id="" name="" type="text" onkeyup="this.setAttribute('value', this.value);" value="" autocomplete="off"></textarea>
+                            <span for="username" class="line__placeholder"> Group description </span> 
+                        </div>
+
+                        <div class="px-2 space-y-2">
+                            <label for="" class="font-semibold text-base"> Choose Privacy </label> 
+                            <div> Anyone can see who's in the group and what they post. </div>
+                            <select id="" name=""  class="shadow-none selectpicker with-border">
+                                <option data-icon="uil-bullseye"> Private </option>
+                                <option data-icon="uil-chat-bubble-user">My Following</option>
+                                <option data-icon="uil-layer-group-slash">Unlisted</option>
+                                <option data-icon="uil-globe" selected>Puplic</option>
+                            </select>
+                        </div>
+                        
+                    </div>
+                    
+                    <!-- form footer -->
+                    <div class="flex justify-center border-gray-100 py-8">
+                        <button type="button" class="button lg:w-1/3">
+                            Create Now
+                        </button>
+                    </div>
+                    
+                </div>
+
         
-            <div class="font-semibold md:text-2xl text-2xl text-gray-700">Your content in our services</div>
-            <div class="md:leading-8 leading-7"> Some of our Services allow you to upload, submit, store, send or receive content. You retain ownership of any intellectual property rights that you hold in that content. In short, what belongs to you stays yours.</div>
-            <div class="md:leading-8 leading-7"> When you upload, submit, store, send or receive content to or through our Services, you give Front (and those we work with) a worldwide license to use, host, store, reproduce, modify, create derivative works (such as those resulting from translations, adaptations or other changes we make so that your content works better with our Services), communicate, publish, publicly perform, publicly display and distribute such content. The rights you grant in this license are for the limited purpose of operating, promoting, and improving our Services, and to develop new ones. This license continues even if you stop using our Services (for example, for a business listing you have added to Front Maps). Some Services may offer you ways to access and remove content that has been provided to that Service. Also, in some of our Services, there are terms or settings that narrow the scope of our use of the content submitted in those Services.</div>
-            
-            <div class="font-semibold md:text-2xl text-xl text-gray-700"> Pay Attention </div>
-            <div class="md:leading-8 leading-7"> Our Services are very diverse, so sometimes additional terms or product requirements (including age requirements) may apply. Additional terms will be available with the relevant Services, and those additional terms become part of your agreement with us if you use.</div>
-        
+            </div>
         </div>
-
-    </div>
-         
-</div>
-</div>
 </template>
